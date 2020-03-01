f11f2ea083c538a721b5cf27c42e7f1563f95755
Vincent Simone <vsimone67@gmail.com>
2020-03-01 10:47:26 -0500

    Updated config files to remove config/secret first

--- FitnessTracker.Service.Diet/k8/createconfigs.ps1
+++ FitnessTracker.Service.Diet/k8/createconfigs.ps1
@@ -1,3 +1,7 @@
+kubectl delete secret appsettings-secret-dietservice --namespace fitnesstracker
+
+kubectl delete configmap appsettings-dietservice --namespace fitnesstracker
+
 kubectl create secret generic appsettings-secret-dietservice --namespace fitnesstracker --from-file=../../configfiles/diet/appsettings.secrets.json
 
 kubectl create configmap appsettings-dietservice --namespace fitnesstracker --from-file=../../configfiles/diet/appsettings.json
--- FitnessTracker.Service.Workout/k8/createconfigs.ps1
+++ FitnessTracker.Service.Workout/k8/createconfigs.ps1
@@ -1,3 +1,7 @@
+kubectl delete secret appsettings-secret-workoutservice --namespace fitnesstracker
+ 
+kubectl delete configmap appsettings-workoutservice --namespace fitnesstracker
+
 kubectl create secret generic appsettings-secret-workoutservice --namespace fitnesstracker --from-file=../../configfiles/workout/appsettings.secrets.json
 
 kubectl create configmap appsettings-workoutservice --namespace fitnesstracker --from-file=../../configfiles/workout/appsettings.json
